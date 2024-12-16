2cfce12a8a446f03d1d2770d6942879fcff00ef2
Manuel <manuelusero6@gmail.com>
2024-12-16 20:06:11 -0300

    Agrego de css

--- src/Components/AddContact/AddContact.jsx
+++ src/Components/AddContact/AddContact.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './AddContact.css';
 
 const AddContact = ({ addContact }) => {
     const [contact, setContact] = useState({ name: '', email: '', phone: '' });
