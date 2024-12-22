d1df4657f77bdb53f09bc6b5dc3dbd9f1658f6ba
Manuel <manuelusero6@gmail.com>
2024-12-22 12:53:17 -0300

    Resolviendo problemas con el acceso a Contacts

--- src/Components/verifyAccount/VerifyAccount.jsx
+++ src/Components/verifyAccount/VerifyAccount.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import "./VerifyAccount.css";
 
 const VerifyAccount = () => {
     const { token } = useParams();
